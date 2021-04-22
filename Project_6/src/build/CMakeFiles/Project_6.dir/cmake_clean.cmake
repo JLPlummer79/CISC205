1fada2f6a2da0c00e7a98279e9ea300617300ce0
Jesse <bfantastic@gmail.com>
2021-04-21 22:20:48 -0700

    "Up to date"

---
+++ Project_6/src/build/CMakeFiles/Project_6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_6.dir/main.cpp.o"
+  "Project_6.exe"
+  "Project_6.exe.manifest"
+  "Project_6.pdb"
+  "libProject_6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Project_6/src/build/CMakeFiles/process-Project_6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/process-Project_6.dir/Powerboat.cpp.o"
+  "CMakeFiles/process-Project_6.dir/Sailboat.cpp.o"
+  "CMakeFiles/process-Project_6.dir/Yacht.cpp.o"
+  "libprocess-Project_6.a"
+  "libprocess-Project_6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/process-Project_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
