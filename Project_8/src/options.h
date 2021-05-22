9aeb639a9bf235ee27344a8ac19a3c2c59a4f644
Jesse <bfantastic@gmail.com>
2021-05-22 14:02:49 -0700

    "Options source file added"

---
+++ Project_8/src/options.h
@@ -0,0 +1,21 @@
+/**
+    Jesse Plummer
+    CISC 205
+    05/29/2021
+    Programming Challenge 8 Re-visiting Ivo's Yacht Painting Service
+*/
+
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+#include "Painting.h"
+#include <vector>
+
+void addCustomer(<Painting>);
+
+void editCustomer(<Painting>);
+
+void findLargest(<Painting>);
+
+
+#endif
